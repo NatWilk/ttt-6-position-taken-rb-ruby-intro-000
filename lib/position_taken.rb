1a475a372949f03cfc9c1119945a03d2b1f5b328
Natalia Wilkus <wilkus.natalia7@gmail.com>
2021-01-25 11:32:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == "X" || "O"
-    true
-  else
-    if board[index] == " " || "" || nil
-      false
-    end
+    
   end
 end
