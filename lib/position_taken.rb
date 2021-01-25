67587a88f97ffa5e2b335e6e908f8417329e6842
Natalia Wilkus <wilkus.natalia7@gmail.com>
2021-01-25 11:26:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == "X" or "0"
+  if board[index] == "X" or "O"
     true
   end
   if board[index] == " "
