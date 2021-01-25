d327a38b00748f97094bd339da4f1d91c852e1f8
Natalia Wilkus <wilkus.natalia7@gmail.com>
2021-01-25 10:52:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if (board[index] == " " || "" || nil) && (board[index] != "X" && "0")
     false
-  elsif board[index] == "X" || "0"
+  elsif board[index] == "X" || "O"
     true
   end
 end
